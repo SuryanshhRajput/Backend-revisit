d2c8ec04c9d7a17ef0f8fbb96f28ee1e3b2c0256
Suryansh Rajput <suryansh@Suryanshs-MacBook-Air.local>
2026-09-17 21:36:48 +0530

    multer done

---
+++ Day8-fileHandling/src/routes/file.route.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const upload = require("../config/multer");
+
+const router = express.Router();
+
+router.post("/", upload.single("image"), (req, res) => {
+  try {
+    let body = req.body;
+    let file = req.file;
+    console.log(file)
+    console.log(body);
+
+    res.status(200).json({
+      message: "file received successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "internal sever error",
+    });
+  }
+});
+
+module.exports = router;
